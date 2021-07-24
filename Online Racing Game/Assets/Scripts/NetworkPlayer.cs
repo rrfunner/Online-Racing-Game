e76a29f1471131fc6fe1394153ef4af953c39801
rrfunner <74343362+rrfunner@users.noreply.github.com>
2021-07-24 22:20:10 +0530

    Revert "Kaam tamaam"
    This reverts commit 12bcdad82738a742217403f02835c21f61c12972.

--- Online Racing Game/Assets/Scripts/NetworkPlayer.cs
+++ Online Racing Game/Assets/Scripts/NetworkPlayer.cs
@@ -1,35 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NetworkPlayer : Photon.MonoBehaviour 
 {
-   public GameObject localcam;
-    private PhotonView pv;
-    
-
+    public GameObject localcam;
     // Start is called before the first frame update
     void Start()
     {
-        pv = GetComponent<PhotonView>();
-        if (!pv.isMine)
-        {          
-         localcam.SetActive(false);
+        if (!photonView.isMine)
+        {
+            localcam.SetActive(false);
 
-            MonoBehaviour[] script = GetComponents<MonoBehaviour>();
-            for( int i = 0;i < script.Length; i++)
-            {
-                if (script[i] is NetworkPlayer) continue;
-                else if(script[i] is PhotonView) continue;
-                script[i].enabled = false;
+            MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+        for(int i = 0; i<scripts.Length; i++)
+         {
+              if (scripts[i] is NetworkPlayer) continue;
+              else if (scripts[i] is PhotonView) continue;
+
+              scripts[i].enabled = false;
           }
-        }
+       }
        
-    }
-    private void Update()
-    {
-        if (!pv.isMine) return;
     }
 
 }
